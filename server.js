daec46dfeb8bce99806c97d0c2b948fe502bdc38
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2022-09-21 13:39:30 -0500

    adding user functionality

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 const {animals} = require('./data/animals');
 const express = require('express');
 const e = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+    // parse incoming string or array data
+    app.use(express.urlencoded({ extended: true }));
+    // parse incoming JSON data
+    app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -43,10 +50,23 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
-  function findById(id, animalsArray) {
+function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
-  }
+};
+
+function createNewAnimal (body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+
+    //return finished code to post route for response
+    return animal;
+}
 
 //route to find an animal array, a wide set of data
 app.get('/api/animals', (req, res) => {
@@ -70,13 +90,13 @@ app.get('/api/animals/:id', (req, res)=> {
 
 //route to post user's input
 app.post('/api/animals', (req, res) => {
-    // parse incoming string or array data
-    app.use(express.urlencoded({ extended: true }));
-    // parse incoming JSON data
-    app.use(express.json());
+    //setting animal id by the next index available, to prevent double of them
+    req.body.id= animals.length.toString()
+    
+    //add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
 
-    //req body is where our incoming content will be
-    console.log(req.body);
+
     res.json(req.body);
 
 });
