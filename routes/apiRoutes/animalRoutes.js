3290b80aee91b801659ca35276f9ea1560fec2f5
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2022-09-22 13:00:52 -0500

    refractoring codes

---
+++ routes/apiRoutes/animalRoutes.js
@@ -0,0 +1,41 @@
+const {filterByQuery, findById, createNewAnimal, validateAnimal} = require('../../lib/animals');
+const { animals } = require('../../data/animals');
+const router = require('express').Router();
+
+
+//route to find an animal array, a wide set of data
+route.get('/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+  });
+
+
+//route to find an animal by ID, much more specific
+route.get('/animals/:id', (req, res)=> {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+//route to post user's input
+route.post('/animals', (req, res) => {
+    //setting animal id by the next index available, to prevent double of them
+    req.body.id= animals.length.toString()
+    
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+
+});
+
+module.exports = router;
