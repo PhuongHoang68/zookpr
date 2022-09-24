884a586ee754867f05c2645f06f1b3c8a582ba1c
PhuongHoang68 <109717048+PhuongHoang68@users.noreply.github.com>
2022-09-24 13:19:25 -0500

    more front-end functionality

--- public/assets/js/zookeeper.js
+++ public/assets/js/zookeeper.js
@@ -1,4 +1,6 @@
 const $displayArea = document.querySelector('#display-area');
+const $zookeeperForm = document.querySelector("#zookeeper-form");
+
 
 const printResults = resultArr => {
   console.log(resultArr);
@@ -39,6 +41,21 @@ const getZookeepers = (formData = {}) => {
       printResults(zookeeperArr);
     });
 };
+
+const handleGetZookeepersSubmit = event => {
+  event.preventDefault();
+  const nameHTML = $zookeeperForm.querySelector('[name="name"]');
+  const name = nameHTML.value;
+
+  const ageHTML = $zookeeperForm.querySelector('[name="age"]');
+  const age = ageHTML.value;
+
+  const zookeeperObject = { name, age };
+
+  getZookeepers(zookeeperObject);
+};
+
+$zookeeperForm.addEventListener('submit', handleGetZookeepersSubmit);
 
 getZookeepers();
 
